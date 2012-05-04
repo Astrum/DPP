becd8548831eed00efeb9b103f3438e5683531d0
Jack Mitchell <astrumvicis@hotmail.com>
2012-05-05 01:27:37 +1000

    The basic panel of 6 dice.

---
+++ Dice Panel/Dice Panel/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DicePanel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.OutPut = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Critical = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "D4"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(94, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "D6"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(175, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "D8"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(256, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "D10"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(94, 41)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "D12"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(175, 41)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "D20"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'OutPut
+        '
+        Me.OutPut.AutoSize = True
+        Me.OutPut.Font = New System.Drawing.Font("Maynard", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OutPut.Location = New System.Drawing.Point(142, 116)
+        Me.OutPut.Name = "OutPut"
+        Me.OutPut.Size = New System.Drawing.Size(0, 69)
+        Me.OutPut.TabIndex = 6
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(118, 90)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'Critical
+        '
+        Me.Critical.AutoSize = True
+        Me.Critical.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Critical.ForeColor = System.Drawing.Color.Red
+        Me.Critical.Location = New System.Drawing.Point(35, 185)
+        Me.Critical.Name = "Critical"
+        Me.Critical.Size = New System.Drawing.Size(273, 63)
+        Me.Critical.TabIndex = 8
+        Me.Critical.Text = "CRITICAL"
+        Me.Critical.Visible = False
+        '
+        'DicePanel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 273)
+        Me.Controls.Add(Me.Critical)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.OutPut)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximumSize = New System.Drawing.Size(353, 300)
+        Me.MinimumSize = New System.Drawing.Size(353, 200)
+        Me.Name = "DicePanel"
+        Me.Text = "Dice Panel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents OutPut As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Critical As System.Windows.Forms.Label
+
+End Class
---
+++ Dice Panel/Dice Panel/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class DicePanel
+    Dim MaxDie As Integer
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MaxDie = 4
+        OutPut.Text = Int(Rnd() * 4 + 1)
+        ProgressBar1.Maximum = 4
+        ProgressBar1.Value = OutPut.Text
+        If OutPut.Text = MaxDie Then
+            ProgressBar1.ForeColor = Color.Crimson
+            Critical.Visible = True
+        ElseIf OutPut.Text = 1 Then
+            ProgressBar1.ForeColor = Color.White
+            Critical.Visible = False
+        Else : ProgressBar1.ForeColor = Color.Blue
+            Critical.Visible = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        MaxDie = 6
+        OutPut.Text = Int(Rnd() * 6 + 1)
+        ProgressBar1.Maximum = 6
+        ProgressBar1.Value = OutPut.Text
+        If OutPut.Text = MaxDie Then
+            ProgressBar1.ForeColor = Color.Crimson
+            Critical.Visible = True
+        ElseIf OutPut.Text = 1 Then
+            ProgressBar1.ForeColor = Color.White
+            Critical.Visible = False
+        Else : ProgressBar1.ForeColor = Color.Blue
+            Critical.Visible = False
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        MaxDie = 8
+        OutPut.Text = Int(Rnd() * 8 + 1)
+        ProgressBar1.Maximum = 8
+        ProgressBar1.Value = OutPut.Text
+        If OutPut.Text = MaxDie Then
+            ProgressBar1.ForeColor = Color.Crimson
+            Critical.Visible = True
+        ElseIf OutPut.Text = 1 Then
+            ProgressBar1.ForeColor = Color.White
+            Critical.Visible = False
+        Else : ProgressBar1.ForeColor = Color.Blue
+            Critical.Visible = False
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        MaxDie = 10
+        OutPut.Text = Int(Rnd() * 10 + 1)
+        ProgressBar1.Maximum = 10
+        ProgressBar1.Value = OutPut.Text
+        If OutPut.Text = MaxDie Then
+            ProgressBar1.ForeColor = Color.Crimson
+            Critical.Visible = True
+        ElseIf OutPut.Text = 1 Then
+            ProgressBar1.ForeColor = Color.White
+            Critical.Visible = False
+        Else : ProgressBar1.ForeColor = Color.Blue
+            Critical.Visible = False
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        MaxDie = 12
+        OutPut.Text = Int(Rnd() * 12 + 1)
+        ProgressBar1.Maximum = 12
+        ProgressBar1.Value = OutPut.Text
+        If OutPut.Text = MaxDie Then
+            ProgressBar1.ForeColor = Color.Crimson
+            Critical.Visible = True
+        ElseIf OutPut.Text = 1 Then
+            ProgressBar1.ForeColor = Color.White
+            Critical.Visible = False
+        Else : ProgressBar1.ForeColor = Color.Blue
+            Critical.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        MaxDie = 20
+        OutPut.Text = Int(Rnd() * 20 + 1)
+        ProgressBar1.Maximum = 20
+        ProgressBar1.Value = OutPut.Text
+        If OutPut.Text = MaxDie Then
+            ProgressBar1.ForeColor = Color.Crimson
+            Critical.Visible = True
+        ElseIf OutPut.Text = 1 Then
+            ProgressBar1.ForeColor = Color.White
+            Critical.Visible = False
+        Else : ProgressBar1.ForeColor = Color.Blue
+            Critical.Visible = False
+        End If
+    End Sub
+End Class
